3dcfb5b0c5c500de589f1d1cb895a8752d406968
Alan Christian <alanchristiancreator@hotmail.com>
2022-03-14 13:30:17 -0300

    chore: Open/Close search form popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,8 +31,22 @@ const toggleMenu = () => {
 menuToggleIcon.addEventListener('click', toggleMenu)
 
 // Open/Close search form popup
+const formOpenBtn = selectElement('#search-icon')
+const formCloseBtn = selectElement('#form-close-btn')
+const searchFormContainer = selectElement('#search-form-container')
+
+formOpenBtn.addEventListener('click', () =>
+  searchFormContainer.classList.add('activated')
+)
+formCloseBtn.addEventListener('click', () =>
+  searchFormContainer.classList.remove('activated')
+)
 
 // -- Close the search form popup on ESC keypress
+// https://keycode.info/ (Referencia)
+window.addEventListener('keyup', event => {
+  if (event.key === 'Escape') searchFormContainer.classList.remove('activated')
+})
 
 // Switch theme/add to local storage
 const bodyElement = document.body
